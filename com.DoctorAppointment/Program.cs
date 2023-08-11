5270f6d206ac32a7221868ee19064172e1b9c632
Binod sapota <binodsapkota96@gmail.com>
2023-08-11 07:21:43 +0545

    space added

--- com.DoctorAppointment/Program.cs
+++ com.DoctorAppointment/Program.cs
@@ -11,6 +11,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
